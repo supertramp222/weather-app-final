c9e75727e1b718cd84d3be9b628fa97a9a684f5b
supertramp222 <38687331+supertramp222@users.noreply.github.com>
2022-07-03 22:33:12 +0200

    Added forecast data from API

--- src/app.js
+++ src/app.js
@@ -18,7 +18,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -56,6 +57,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "6ad09e88b3e793860ef68d84c8bf5d66";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   //City+Temperature+Description
   let temperatureElement = document.querySelector("#temperature");
@@ -77,6 +85,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -100,7 +110,6 @@ function searchLocation(position) {
   let apiKey = "6ad09e88b3e793860ef68d84c8bf5d66";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
 
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayTemperature);
 }
 
